d3e6e7aee0451a3cf308f1cec3f25955763a7e16
DarthShoge <Shogeluk@gmail.com>
2014-10-29 00:10:49 +0000

    made several view changes

--- Projects/Musex.Analytics/Muszex.Web/app/artist/artistController.js
+++ Projects/Musex.Analytics/Muszex.Web/app/artist/artistController.js
@@ -1,11 +1,21 @@
 ﻿
+(function () {
+    var app = angular.module("artistApp", [])
 
-var artistController = function($scope) {
 
-    var artist = {
-        name: "Bamboo Razack"
-        
+    var artistController = function ($scope) {
+        var artist = {
+            name: "Bamboo Razack",
+            locale: "London, UK",
+            genres: [{ name: "Hip-hop" },
+                { name: "Soul" },
+                { name: "Electronica" }
+            ]
+
+        };
+
+        $scope.artist = artist;
     };
 
-    $scope.artist = artist;
-}
+    app.controller("artistController",["$scope",artistController])
+}())
